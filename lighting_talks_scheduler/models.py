9ae77c50e709d93839621da93c65b0e158296640
Rachel Appiah-Kubi <appir029.208@laretraite.co.uk>
2022-09-21 15:01:53 +0100

    add lightning and person migrations

--- lighting_talks_scheduler/models.py
+++ lighting_talks_scheduler/models.py
@@ -2,16 +2,16 @@ from django.db import models
 import datetime
 
 class Person(models.Model):
-    first_name= models.CharField(max_length=30)
-    last_name= models.CharField(max_length=30)
+    first_name= models.CharField(max_length=100)
+    last_name= models.CharField(max_length=100)
     email= models.EmailField()
 
     def __str__(self):
         return "%s %s" %(self.first_name, self.last_name)
 
-class Lightning(models.Model):
-    user= models.ForeignKey(Person, on_delete=models.CASCADE)
+class LightningTalk(models.Model):
+    user= models.ForeignKey(Person, on_delete=models.PROTECT)
     time= models.TimeField()
     date= models.DateField()
-    title=models.CharField(max_length=50)
-    subject=models.CharField(max_length=100)
+    title=models.CharField(max_length=240)
+    subject=models.CharField(max_length=240)
