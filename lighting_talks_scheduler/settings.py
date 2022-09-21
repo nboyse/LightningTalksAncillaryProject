38783db78a27068c6406e9dae9427879280e1ff0
nboyse <natasha.boyse@digital.trade.gov.uk>
2022-09-21 14:08:52 +0100

    proper setup for frontend

--- lighting_talks_scheduler/settings.py
+++ lighting_talks_scheduler/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static'
 
             ],
         },
@@ -114,13 +115,18 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- lighting_talks_scheduler/urls.py
+++ lighting_talks_scheduler/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', homepage)
 ]
